342fe94a91f9c18b44ef11a72a5ccf890e375780
JPVignet <jpvignet@gmail.com>
2026-06-02 18:05:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chants-cache-v3";
+const CACHE_NAME = "chants-cache-v4";
 
 // Installation : on met en cache les fichiers essentiels
 self.addEventListener("install", event => {
